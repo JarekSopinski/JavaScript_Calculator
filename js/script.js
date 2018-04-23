ba9e2a1ba718f7b0633b0af289f00bfdfc18b862
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-23 12:24:08 +0200

    Cached buttons

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const $displayChain = $("#displayChain");
+const $displayResult = $("#displayResult");
+
+const $btnAC = $("#btnAC");
+const $btnCE = $("#btnCE");
+const $btnEquals = $("#btnEquals");
+
+const $btnAdd = $("#btnAdd");
+const $btnSubtract = $("#btnSubtract");
+const $btnMultiply = $("#btnMultiply");
+const $btnDivide = $("#btnDivide");
+
+const $btn0 = $("#btn0");
+const $btn1 = $("#btn1");
+const $btn2 = $("#btn2");
+const $btn3 = $("#btn3");
+const $btn4 = $("#btn4");
+const $btn5 = $("#btn5");
+const $btn6 = $("#btn6");
+const $btn7 = $("#btn7");
+const $btn8 = $("#btn8");
+const $btn9 = $("#btn9");
+const $btnPeriod = $("#btnPeriod");
