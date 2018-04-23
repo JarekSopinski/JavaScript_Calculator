85626d820927da74120d6281000df7ae0be16a56
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-23 13:30:56 +0200

    Succesfuly calculating with two numbers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+let state = {
+    numbers: {},
+};
+
+const initialState = {
+    numbers: {
+        firstNumber: null,
+        secondNumber: null
+    },
+    operator: null,
+    result: 0,
+    startedCalculating: false
+};
+
 const $displayChain = $("#displayChain");
 const $displayResult = $("#displayResult");
 
@@ -21,3 +35,68 @@ const $btn7 = $("#btn7");
 const $btn8 = $("#btn8");
 const $btn9 = $("#btn9");
 const $btnPeriod = $("#btnPeriod");
+
+const setInitialState = () => state = {...initialState};
+
+const setNumber = number => {
+    !state.startedCalculating ?
+        state.numbers.firstNumber = number
+        :
+        state.numbers.secondNumber = number
+};
+
+const setOperator = operator => {
+    state.operator = operator;
+    state.startedCalculating = true
+};
+
+const calculate = (operator) => {
+
+    let result;
+
+    switch (operator) {
+        case "add":
+            result = state.numbers.firstNumber + state.numbers.secondNumber;
+            break;
+        case "subtract":
+            result = state.numbers.firstNumber - state.numbers.secondNumber;
+            break;
+        case "multiply":
+            result = state.numbers.firstNumber * state.numbers.secondNumber;
+            break;
+        case "divide":
+            result = state.numbers.firstNumber / state.numbers.secondNumber;
+            break
+    }
+
+    state.result = result
+
+};
+
+const getResult = () => {
+
+    calculate(state.operator);
+    console.log(state.result);
+    //render result
+    setInitialState()
+
+};
+
+$btn0.on("click", () => setNumber(0));
+$btn1.on("click", () => setNumber(1));
+$btn2.on("click", () => setNumber(2));
+$btn3.on("click", () => setNumber(3));
+$btn4.on("click", () => setNumber(4));
+$btn5.on("click", () => setNumber(5));
+$btn6.on("click", () => setNumber(6));
+$btn7.on("click", () => setNumber(7));
+$btn8.on("click", () => setNumber(8));
+$btn9.on("click", () => setNumber(9));
+
+$btnAdd.on("click", () => setOperator("add"));
+$btnSubtract.on("click", () => setOperator("subtract"));
+$btnMultiply.on("click", () => setOperator("multiply"));
+$btnDivide.on("click", () => setOperator("divide"));
+
+$btnCE.on("click", setInitialState);
+$btnEquals.on("click", getResult);
